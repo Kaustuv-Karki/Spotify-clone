4cc4987aeba2ad424d963893d98bcbe7a1033ccc
Kaustuv <karkikaustuv@gmail.com>
2020-08-08 21:53:32 +0545

    little changes made

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1zjxjlcjIsqesp6ZLPjUz8lpn2GNIPxw",
+    authDomain: "spotify-clone-b5626.firebaseapp.com",
+    databaseURL: "https://spotify-clone-b5626.firebaseio.com",
+    projectId: "spotify-clone-b5626",
+    storageBucket: "spotify-clone-b5626.appspot.com",
+    messagingSenderId: "203733670589",
+    appId: "1:203733670589:web:f10b176b3f2f8762c675d4",
+    measurementId: "G-3R07ZGMSP6"
+};
+  
