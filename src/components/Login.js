e0590a43e6735c405ee6d328a3415a488ad6b1d6
Kaustuv <karkikaustuv@gmail.com>
2020-08-09 22:08:33 +0545

    react with spotify api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Login.css'
+import {accessUrl} from '../spotify'
 
 const Login = () => {
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo"/>
-            <a>Login With Spotify</a>
+            <a href={accessUrl}>Login With Spotify</a>
         </div>
     )
 }
